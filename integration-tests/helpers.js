2e32046fdd410cd8907f79cacb16c66b93bb6971
Lerena Holloway <holloway.lerena@gmail.com>
2024-07-21 08:11:10 +0100

    first init

---
+++ integration-tests/helpers.js
@@ -0,0 +1,7 @@
+import webdriver from 'selenium-webdriver';
+import chrome from 'chromedriver';
+
+export const getDriver = () => {
+  let driver = new webdriver.Builder().forBrowser('chrome').build();
+  return driver;
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 import './App.css';
+import './styles.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MovieGrid from './components/MovieGrid';
+import Watchlist from './components/Watchlist';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+
+  useEffect(() => {
+    fetch('movies.json')
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const toggleWatchlist = (movieId) => {
+    setWatchlist((prevWatchlist) =>
+      prevWatchlist.includes(movieId)
+        ? prevWatchlist.filter((id) => id !== movieId)
+        : [...prevWatchlist, movieId]
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header />
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <MovieGrid
+                  movies={movies}
+                  watchlist={watchlist}
+                  toggleWatchlist={toggleWatchlist}
+                />
+              }
+            />
+            <Route
+              path="/watchlist"
+              element={
+                <Watchlist
+                  movies={movies}
+                  watchlist={watchlist}
+                  toggleWatchlist={toggleWatchlist}
+                />
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
     </div>
   );
 }
